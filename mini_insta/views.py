450dad96babcb4dab047326c03a91b5db14626a5
vmulu <vmulu@bu.edu>
2025-09-23 13:12:26 -0400

    minor changes

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -22,5 +22,5 @@ class ProfileDetailView(DetailView):
     Displays a single profile
     """
     model = Profile
-    template_name = "mini_insta/profile.html"
+    template_name = "mini_insta/show_profile.html"
     context_object_name = "profile"
