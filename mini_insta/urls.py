095f25e4a8de9fbd50e65b21826ae9e5942930cf
vmulu <vmulu@bu.edu>
2025-09-23 13:05:00 -0400

    completed show_all page second checkpoint

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,4 +1,10 @@
+# File: mini_insta/urls.py
+# Author: Victoria Mulugeta (vmulu@bu.edu), 9/23/2025
+# Description: Receiving HTTP requests and provides the corresponding view function
 
+from django.urls import path
+from . views import ProfileDetailView, ProfileListView
 urlpatterns = [
-    
+    path('', ProfileListView.as_view(), name="show_all_profiles"),
+    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='profile'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,26 @@
+# File: mini_insta/views.py
+# Author: Victoria Mulugeta (vmulu@bu.edu), 9/23/2025
+# Description: defines the view functions and/or class-based views for the Django application.
+
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . models import Profile
 
 # Create your views here.
+
+class ProfileListView(ListView):
+    """
+    Displays a list of all Profile objects in the application.
+    """
+
+    model = Profile
+    template_name = "mini_insta/show_all_profiles.html"
+    context_object_name = "profiles"
+
+class ProfileDetailView(DetailView):
+    """
+    Displays a single profile
+    """
+    model = Profile
+    template_name = "mini_insta/profile.html"
+    context_object_name = "profile"
