f4d1af3a3bf6165007f9dc5c61ff656270f2e4d7
vmulu <vmulu@bu.edu>
2025-09-25 11:38:28 -0400

    completed assignment

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -15,6 +15,7 @@ class Profile(models.Model):
     display_name = models.TextField(blank=True)
     profile_image_url = models.URLField(blank=True)
     join_date = models.DateTimeField(auto_now=True)
+    bio_text = models.TextField(blank=True)
 
     def __str__(self):
         return f'username: {self.username} name: {self.display_name}'
